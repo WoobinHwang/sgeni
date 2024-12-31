b56b0ad34de4d831a5faa6c04f9aa5b72b8ee56f
WoobinHwang <dnqls0326@naver.com>
2024-12-31 13:22:15 +0900

    settine base

--- src/test/java/com/game/sgeni/SgeniApplicationTests.java
+++ src/test/java/com/game/sgeni/SgeniApplicationTests.java
@@ -1,5 +1,9 @@
 package com.game.sgeni;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +11,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SgeniApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void dbConnecTest() {
+		Connection dbconn = null;
+	    String dbUrl = "jdbc:postgresql://ep-dark-boat-a4da7k40.us-east-1.pg.koyeb.app/koyebdb";
+	    String dbUser = "game";
+	    String dbPassword = "B7aLPlKt2YgW"; 
+	    
+		Connection connection = null;
+        
+        try 
+        {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(
+            		dbUrl, 
+            		dbUser, 
+            		dbPassword);
+            dbconn = connection;
+            
+            System.out.println("DB Connection [성공]");
+        } 
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+        } 
+        catch (ClassNotFoundException e) 
+        {
+            e.printStackTrace();
+        }
+	    
 	}
 
 }
